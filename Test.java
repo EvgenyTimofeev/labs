f223ffa65c31368f7b1c31955a96dfa6958edb40
EvgenyTimofeev <etjava@yandex.ru>
2014-04-05 03:47:47 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,11 +4,13 @@
 public class Test {
     public static void main(String[] args) {
         LinkedStack<Integer> myStack = new LinkedStack<Integer>();
+        myStack.push(null);
         myStack.push(1);
         myStack.push(2);
         myStack.push(3);
         myStack.push(4);
         myStack.push(5);
+        myStack.push(null);
         System.out.println(myStack);
         System.out.println("peek " + myStack.peek());
         System.out.println("pop " + myStack.pop());
